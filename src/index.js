75c21c9541061b2a990998a970c0f70d6d8a2adc
dumperize <ole.elena@gmail.com>
2016-11-09 01:33:26 +0400

    second

--- src/index.js
+++ src/index.js
@@ -20,10 +20,37 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.get('/', function (req, res) {
+app.get('/skb3', function (req, res) {
     const a = req.query.a || 0;
     const b = req.query.b || 0;
-    res.send((+a + +b).toString());
+    return res.send((+a + +b).toString());
+});
+
+app.get('/skb4', function (req, res) {
+    const fullname = req.query.fullname;
+
+    if (!fullname || fullname.search(/[0-9_\/]/i) != -1)
+        return res.send('Invalid fullname');
+
+    const fullnameArr = fullname.trim().split(/\s+/).map(s => {
+        return s.charAt(0).toUpperCase() + s.toLowerCase().substr(1);
+    });
+    let name;
+    switch (fullnameArr.length) {
+        case 3:
+            name = fullnameArr[2] + ' ' + fullnameArr[0].charAt(0) + '. ' + fullnameArr[1].charAt(0) + '.';
+            break;
+        case 2:
+            name = fullnameArr[1] + ' ' + fullnameArr[0].charAt(0) + '.';
+            break;
+        case 1:
+            name = fullnameArr[0];
+            break;
+        default:
+            name = 'Invalid fullname';
+
+    }
+    return res.send(name);
 });
 
 app.listen(3000, function () {
