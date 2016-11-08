0788eb5407565e7432e0a0ac882db5d35bd142bd
dumperize <ole.elena@gmail.com>
2016-11-08 16:31:01 +0400

    first

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+const app = express();
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+app.get('/', function (req, res) {
+    const a = req.query.a || 0;
+    const b = req.query.b || 0;
+    res.send((+a + +b).toString());
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
